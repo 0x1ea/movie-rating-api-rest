006a84b273de0c9c49f6a9eb5b0979cd0c4fc2f2
Ardy <cryptoardy@gmail.com>
2022-06-22 10:20:20 -0400

    get data from the api with fetch

--- components/CategoriesPreview.js
+++ components/CategoriesPreview.js
@@ -1,28 +1,43 @@
 import React from "react"
 import styles from "../styles/CategoriesPreview.module.css"
+
 const CategoriesPreview = () => {
+  const [categories, setCategories] = React.useState([])
+  const API_KEY = process.env.API_KEY
+  const API_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+
+  const getCategoriesPreview = async () => {
+    const res = await fetch(API_URL)
+    const data = await res.json()
+
+    const categories = data.genres
+
+    setCategories(categories)
+    console.log(categories)
+  }
+
+  React.useEffect(() => {
+    getCategoriesPreview()
+  }, [])
+
   return (
     <section id="categoriesPreview" className={styles.categoriesPreview_container}>
       <h2 className={styles.categoriesPreview_title}>Categorías</h2>
 
       <article className={styles.categoriesPreview_list}>
-        <div className={styles.category_container}>
-          <h3 id="id28" className={styles.category_title}>
-            Romance
-          </h3>
-        </div>
+        {categories.map((genre) => (
+          <div key={genre.id} className={styles.category_container}>
+            <h3 id={"id" + genre.id} className={styles.category_title}>
+              {genre.name}
+            </h3>
+          </div>
+        ))}
 
-        <div className={styles.category_container}>
-          <h3 id="id16" className={styles.category_title}>
-            Drama
-          </h3>
-        </div>
-
-        <div className={styles.category_container}>
+        {/* <div className={styles.category_container}>
           <h3 id="id14" className={styles.category_title}>
             Acción
           </h3>
-        </div>
+        </div> */}
       </article>
     </section>
   )
--- components/Trendring.js
+++ components/Trendring.js
@@ -3,43 +3,43 @@ import styles from "../styles/Trending.module.css"
 import Image from "next/image"
 
 const Trendring = () => {
+  const [movies, setMovies] = React.useState([])
+  const API_KEY = process.env.API_KEY
+  const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+
+  const getTrendingMoviesPreview = async () => {
+    const res = await fetch(API_URL)
+    const data = await res.json()
+
+    const movies = data.results
+
+    setMovies(movies)
+    console.log(movies)
+  }
+
+  React.useEffect(() => {
+    getTrendingMoviesPreview()
+  }, [])
+
   return (
-    <section id="trendingPreview" className={styles.trendingPreview_container}>
+    <section className={styles.trendingPreview_container}>
       <div className={styles.trendingPreview_header}>
         <h2 className={styles.trendingPreview_title}>Tendencias</h2>
         <button className={styles.trendingPreview_btn}>Ver más</button>
       </div>
 
       <article className={styles.trendingPreview_movieList}>
-        <div className={styles.movie_container}>
-          <Image
-            src="https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg"
-            className={styles.movie_img}
-            alt="Nombre de la película"
-            width={150}
-            height={225}
-          />
-        </div>
-
-        <div className={styles.movie_container}>
-          <Image
-            src="https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg"
-            className={styles.movie_img}
-            alt="Nombre de la película"
-            width={150}
-            height={225}
-          />
-        </div>
-
-        <div className={styles.movie_container}>
-          <Image
-            src="https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg"
-            className={styles.movie_img}
-            alt="Nombre de la película"
-            width={150}
-            height={225}
-          />
-        </div>
+        {movies.map((movie) => (
+          <div key={movie.id} className={styles.movie_container}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              className={styles.movie_img}
+              alt={movie.title}
+              width={150}
+              height={225}
+            />
+          </div>
+        ))}
       </article>
     </section>
   )
